e287005a35dbbc9b304f009a3c2fecfceae5bdc9
Takoua Kaaouana <kaaouana.takoua@gmail.com>
2020-11-30 21:15:45 +0100

    add sql exercice

--- EX4/Code/app.py
+++ EX4/Code/app.py
@@ -1,14 +1,20 @@
+# flask_web/app.py
+
+From flask import Flask
+app = Flask(__name__)
+
+
 import sys
 from SparseArray import SparseArray
 import os
 
 
-
+@app.route('/')
 def main(config_arg): 
   print("Start exercice 'Maisons du monde'")
-  strings=['ab','abc','ab','adc','adc','aa','bb','ab']
+  strings=['ab','adc','ab','adc','adc','aa','bb','ab']
   x = SparseArray(strings)
-  if True :
+  if False :
 	  import os 
 	  print("using config for {}".format(config_arg))
 	  print (config_arg)
@@ -16,13 +22,13 @@ def main(config_arg):
 	  print (queries[0])
 	  queries = queries[0].split(",")
 	  print (queries)
-	  print(x.matchingStrings(queries))
 	  #do decompose element to multi-variable by ','
   queries=['abk','adc','ab']
   print(x.matchingStrings(queries))
+  return x.matchingStrings(queries)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    app.run(debug=True, host='0.0.0.0')
 	
 
